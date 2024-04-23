bf8cf57e32ddf379882f3ed676ee9e2814fbb1ae
Amit sharma <amitshar82@gmail.com>
2024-04-23 23:44:34 +0530

    Update app.js

--- Node/app.js
+++ Node/app.js
@@ -4,18 +4,12 @@ const cors = require('cors')
 const router = require('./router');
 const PORT = process.env.PORT || 4000;
 
-
-
 app.use(express.json());
 app.use(cors({
     "Access-Control-Allow-Origin":"*"
-}))
-
-
+}));
 
 app.use('/', router);
-
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
